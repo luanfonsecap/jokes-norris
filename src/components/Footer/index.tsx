5651014557af240dab85f7d0e61858aae7e46a6f
luanfonsecap <luanfonsecap@icloud.com>
2020-05-06 01:06:07 -0300

    created footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+      <p>Jokes powered by <a target="blank" href="https://api.chucknorris.io/">Chuck Norris IO</a></p>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  width: 100%;
+  height: 70px;
+
+  background: #fff;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  p {
+    font-size: 1rem;
+    font-weight: 300;
+
+    a {
+      font-size: 1rem;
+      font-weight: bold;
+      text-decoration: none;
+    }
+  }
+
+  @media (min-width: 768px){
+    position: absolute;
+    bottom: 0;
+  }
+`;
