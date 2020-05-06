8d65f98eaee21d75dc6b32e0a41f0e4bc15efcce
luanfonsecap <luanfonsecap@icloud.com>
2020-05-06 16:02:40 -0300

    add reload joke logic

--- src/pages/Joke/index.tsx
+++ src/pages/Joke/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import Typist from 'react-typist';
+import ReactLoading from 'react-loading';
 
 import logoImg from '../../assets/logo.svg';
 import reloadImg from '../../assets/reload.svg';
@@ -19,12 +20,23 @@ const Joke: React.FC = () => {
     loadJoke(category);
   }, [loadJoke, category]);
 
+  const reloadJoke = useCallback(() => {
+    setClicked(true);
+    loadJoke(category);
+    setTimeout(() => {
+      setClicked(false);
+    }, 1000);
+  }, [loadJoke, category]);
+
   return (
     <>
       <Container>
         <img src={logoImg} alt="Chuck Norris Character" />
 
         <JokeBox>
+          {!joke.value && (
+            <ReactLoading type="bars" color="#F2591F" height={56} width={56} />
+          )}
           {joke.value && (
             <Typist avgTypingDelay={60} cursor={{ hideWhenDone: true }}>
               {joke.value}
