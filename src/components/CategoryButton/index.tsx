21fc32557f038c9e4a72b6558ad14cae5f5f2685
luanfonsecap <luanfonsecap@icloud.com>
2020-05-05 22:52:28 -0300

    created category button

---
+++ src/components/CategoryButton/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const CategoryButton: React.FC = () => {
+  return (
+    <Container>
+      <span>Animal</span>
+    </Container>
+  );
+}
+
+export default CategoryButton;
---
+++ src/components/CategoryButton/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  background: #24ADE3;
+  border: none;
+  border-radius: 4px;
+  box-shadow: 0 0 4px #8f8f8f;
+
+  padding: 16px;
+  min-width: 156px;
+
+  text-align: center;
+
+  span {
+    color: #fff;
+    font-size: 1rem;
+  }
+`;
