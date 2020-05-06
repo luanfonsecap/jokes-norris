26ae82e2fa9d0b3b10a82f554ead1aabba927816
luanfonsecap <luanfonsecap@icloud.com>
2020-05-06 01:07:34 -0300

    add header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,26 +2,42 @@ import React from 'react';
 
 import Logo from '../../components/Logo'
 import CategoryButton from '../../components/CategoryButton';
+import Footer from '../../components/Footer';
+
 import { Container } from './styles';
 
 const Home: React.FC = () => {
-  return <Container>
-    <header>
-      <Logo />
-      <p>Select a categorie for a random Chuck's joke</p>
-    </header>
+  return (
+    <>
+      <Container>
+        <header>
+          <Logo />
+          <p>Select a categorie for a random Chuck's joke</p>
+        </header>
 
-    <main>
-      <CategoryButton />
-      <CategoryButton />
-      <CategoryButton />
-      <CategoryButton />
-      <CategoryButton />
-      <CategoryButton />
-      <CategoryButton />
-      <CategoryButton />
-    </main>
-  </Container>;
+        <main>
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+          <CategoryButton />
+        </main>
+      </Container>
+      <Footer />
+    </>
+  );
+
 }
 
 export default Home;
