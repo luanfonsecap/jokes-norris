21f558118c08438e5e843d54a1863f27aa2451a7
Luan Fonseca <luanfonsecap@icloud.com>
2021-10-03 21:25:29 -0300

    feat: add recursive api call to not repeat jokes

--- src/modules/Joke/hooks/useJoke.tsx
+++ src/modules/Joke/hooks/useJoke.tsx
@@ -1,9 +1,14 @@
-import React, { createContext, useContext, useCallback, useState } from 'react';
+import { createContext, useContext, useCallback, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
+import { AppSignatureStorage } from '../../common/constants/storage';
+import { JokeResponse } from '../../common/services/interfaces/JokeResponse';
 import JokesService from '../../common/services/jokesService';
 import { WithChildren } from '../../common/utils/WithChildren';
 
 interface AddJokeData {
+  id: string;
   categories: string[];
   value: string;
 }
@@ -18,18 +23,77 @@ const JokeContext = createContext<JokeContextData>({} as JokeContextData);
 
 const JokeProvider = ({ children }: WithChildren) => {
   const [joke, setJoke] = useState<AddJokeData>({
+    id: '',
     categories: [],
     value: '',
   });
   const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
-  const loadJoke = useCallback(async category => {
-    const jokesService = new JokesService();
-    setLoading(true);
-    const response = await jokesService.getRandomJokeByCategory(category);
-    setJoke(response);
-    setLoading(false);
-  }, []);
+  const recursiveLoadNewJoke = useCallback(
+    async (
+      category: string,
+      loadJokeRequest: (arg: string) => Promise<JokeResponse | null>,
+      attempts: number,
+    ): Promise<JokeResponse | null> => {
+      if (attempts >= 15) {
+        return null;
+      }
+
+      let attempt = attempts;
+
+      let response = await loadJokeRequest(category);
+      if (response === null) {
+        return null;
+      }
+
+      const jokesAlreadyRead: string[] = JSON.parse(
+        localStorage.getItem(`${AppSignatureStorage}jokesAlreadyRead`) || '[]',
+      );
+
+      if (jokesAlreadyRead.includes(response.id)) {
+        attempt += 1;
+        response = await recursiveLoadNewJoke(
+          category,
+          loadJokeRequest,
+          attempt,
+        );
+      }
+
+      if (response) {
+        jokesAlreadyRead.push(response.id);
+        localStorage.setItem(
+          `${AppSignatureStorage}jokesAlreadyRead`,
+          JSON.stringify(jokesAlreadyRead),
+        );
+      }
+
+      return response || null;
+    },
+    [],
+  );
+
+  const loadJoke = useCallback(
+    async (category: string) => {
+      const jokesService = new JokesService();
+      setLoading(true);
+      const response = await recursiveLoadNewJoke(
+        category,
+        jokesService.getRandomJokeByCategory,
+        0,
+      );
+
+      if (!response) {
+        toast.info('You have read all jokes from this category.');
+        history.push('/');
+        return;
+      }
+
+      setJoke(response);
+      setLoading(false);
+    },
+    [recursiveLoadNewJoke, history],
+  );
 
   return (
     <JokeContext.Provider value={{ joke, loadJoke, loading }}>
