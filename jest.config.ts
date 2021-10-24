ea4001c7a8c3a82835a4150f00d1c77885fec291
Luan Fonseca <luanfonsecap@icloud.com>
2021-10-24 14:29:40 -0300

    fix: jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -143,7 +143,7 @@ export default {
   // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testRegex: '(/src/**/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
