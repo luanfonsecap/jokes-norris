8e8c7cd5834f7a1be6c3fcb793f5815ab7fae917
Luan Fonseca <luanfonsecap@icloud.com>
2022-03-05 19:29:34 -0300

    feat: :sparkles: includes more context to all the providers component

--- src/modules/common/tests/utils/AllTheProviders.tsx
+++ src/modules/common/tests/utils/AllTheProviders.tsx
@@ -1,11 +1,17 @@
+import { JokeProvider } from '../../../Joke/hooks/useJoke';
 import { HomeErrorFallback } from '../../errors/HomeErrorFallback';
+import { JokeErrorFallback } from '../../errors/JokeErrorFallback ';
 import AppProvider from '../../hooks';
 import { WithChildren } from '../../utils/WithChildren';
 
 const AllTheProviders = ({ children }: WithChildren) => {
   return (
     <AppProvider>
-      <HomeErrorFallback>{children}</HomeErrorFallback>
+      <JokeErrorFallback>
+        <JokeProvider>
+          <HomeErrorFallback>{children}</HomeErrorFallback>
+        </JokeProvider>
+      </JokeErrorFallback>
     </AppProvider>
   );
 };
